6eb276b5696ca6866ebfc6dce1711554a82dbdac
priya <pr.priyarawat2012@gmail.com>
2021-12-13 11:53:10 +0530

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//Testng xml file from Maven 
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+
+	}
+
+}
